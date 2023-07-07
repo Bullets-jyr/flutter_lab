682436056bbcbb5f54b128f0e4bcb9024e9a380d
Bullets-jyr <jyr@bullets.co.kr>
2023-07-07 19:18:58 +0900

    05-1 함수 선언과 호출하기

---
+++ lib/ch5_1/test.dart
@@ -0,0 +1,18 @@
+// var a: dynamic
+// var을 생략할 수 있다.
+void some1(var a) {
+
+}
+
+// dynamic some2() {} 와 동일합니다.
+some2() {
+  // return 10;
+  // return true;
+  return 'hello';
+}
+
+void main() {
+  some1(10);
+  some1(true);
+  some2('hello');
+}

5bcdfdf30af4301cd20e265d5eaaed5586e37b8a
Bullets-jyr <jyr@bullets.co.kr>
2023-07-14 21:46:48 +0900

    19-2 다양한 프로바이더 알아보기 #1-ChangeNotifierProvider

---
+++ lib/ch19_2_1_changenotifier/test.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class Counter with ChangeNotifier {
+  int _count = 0;
+
+  int get count => _count;
+
+  void increment() {
+    _count++;
+    notifyListeners();
+  }
+}
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('ChangeNotifierProvider Test'),),
+        body: ChangeNotifierProvider<Counter>.value(
+          value: Counter(),
+          child: SubWidget(),
+        ),
+      ),
+    );
+  }
+}
+
+class SubWidget extends StatelessWidget {
+  const SubWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var counter = Provider.of<Counter>(context);
+    return Container(
+      color: Colors.orange,
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Provider count : ${counter.count}',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
+            ),
+            ElevatedButton(onPressed: () { counter.increment(); }, child: Text('increment'))
+          ],
+        ),
+      ),
+    );
+  }
+}
